fc00747c952e6090e22c98e75009f729469bab8b
peter0749 <jengku@gmail.com>
2018-09-01 17:56:20 +0800

    breast cancer

--- PLA/test_on_libsvm_dataset.sh
+++ PLA/test_on_libsvm_dataset.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/ijcnn1.bz2
-wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/ijcnn1.t.bz2
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/breast-cancer_scale
 
-bzip2 -d ijcnn1.bz2
-bzip2 -d ijcnn1.t.bz2
+dataset_size=$(wc breast-cancer_scale | awk '{print $1}')
+test_size=$((dataset_size/3))
 
-python PLA_libsvm_dataset.py ijcnn1 ijcnn1.t 300
+sort -R breast-cancer_scale > breast-cancer_scale.random
+rm -f breast-cancer_scale
+tail -n $test_size breast-cancer_scale.random > breast-cancer_scale.t
+head -n $((dataset_size-test_size)) breast-cancer_scale.random > breast-cancer_scale
+rm -f breast-cancer_scale.random
 
-rm -f ijcnn1 ijcnn1.t
+python PLA_libsvm_dataset.py breast-cancer_scale breast-cancer_scale.t 800
+
+rm -f breast-cancer_scale breast-cancer_scale.t
 
